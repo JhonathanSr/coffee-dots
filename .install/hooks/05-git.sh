869b044b261e5aa5ce19f3a1174fa41c8106caa7
JhonathanSr <tatanbmxwiw@gmail.com>
2026-06-10 19:09:36 -0500

    Ajustes minimos

--- .install/hooks/05-git.sh
+++ .install/hooks/05-git.sh
@@ -4,6 +4,35 @@
 CYE=$(tput setaf 3); CGR=$(tput setaf 2); BLD=$(tput bold); CNC=$(tput sgr0)
 
 printf "%b\n" "${BLD}${CYE}Inyectando directivas y aliases globales de Git...${CNC}"
+
+# ==========================================
+# Set identification from install inputs
+# ==========================================
+
+# 1. Solicitar y configurar el Nombre de Usuario
+if [[ -z ${USER_NAME//[[:space:]]/} ]]; then
+  read -r -p "Introduce tu nombre para Git (ej. Jhonathan): " USER_NAME
+fi
+
+# 2. Solicitar y configurar el Correo Electrónico
+if [[ -z ${USER_EMAIL//[[:space:]]/} ]]; then
+  read -r -p "Introduce tu email para Git (ej. tu@email.com): " USER_EMAIL
+fi
+
+# Validar que no se haya introducido una cadena vacía tras el prompt
+if [[ -n ${USER_NAME//[[:space:]]/} ]]; then
+  git config --global user.name "$USER_NAME"
+else
+  echo "Log: No se configuró user.name (entrada vacía)."
+fi
+
+if [[ -n ${USER_EMAIL//[[:space:]]/} ]]; then
+  git config --global user.email "$USER_EMAIL"
+else
+  echo "Log: No se configuró user.email (entrada vacía)."
+fi
+
+
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
